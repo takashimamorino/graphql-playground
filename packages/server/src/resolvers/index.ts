d2adb3ebfb7a30cf95a7ac968379a07b895dd140
takashima <does1026@gmail.com>
2021-10-23 11:56:47 +0900

    chore: setup graphql-scalars

--- packages/server/src/resolvers/index.ts
+++ packages/server/src/resolvers/index.ts
@@ -1,4 +1,5 @@
 import { mergeResolvers } from '@graphql-tools/merge';
+import { resolvers as scalarResolvers } from 'graphql-scalars';
 import { bookResolver } from './book';
 
-export const resolvers = mergeResolvers([bookResolver]);
+export const resolvers = mergeResolvers([scalarResolvers, bookResolver]);
--- packages/server/src/schemas/index.ts
+++ packages/server/src/schemas/index.ts
@@ -1,7 +1,9 @@
 import { loadFilesSync } from '@graphql-tools/load-files';
 import { mergeTypeDefs } from '@graphql-tools/merge';
+import { typeDefs as scalarsTypeDefs } from 'graphql-scalars';
 import path from 'path';
 
 const typesArray = loadFilesSync(path.join(__dirname, './**/*.graphql'));
+const types = [scalarsTypeDefs, typesArray];
 
-export const typeDefs = mergeTypeDefs(typesArray);
+export const typeDefs = mergeTypeDefs(types);
