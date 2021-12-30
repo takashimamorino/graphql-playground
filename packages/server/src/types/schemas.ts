81f971c9d8695c754b27f60f43cbedcb08555672
takashimamorino <does1026@gmail.com>
2021-12-31 06:11:21 +0900

    chore: tmp

--- packages/server/src/types/schemas.ts
+++ packages/server/src/types/schemas.ts
@@ -32,6 +32,7 @@ export type Scalars = {
 
 export type Book = {
   __typename?: 'Book';
+  author: Scalars['String'];
   id: Scalars['ID'];
   title: Scalars['String'];
 };
@@ -195,6 +196,7 @@ export type BookResolvers<
   ContextType = any,
   ParentType extends ResolversParentTypes['Book'] = ResolversParentTypes['Book']
 > = {
+  author?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
   id?: Resolver<ResolversTypes['ID'], ParentType, ContextType>;
   title?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
   __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
